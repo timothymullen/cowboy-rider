e6e602ee238b462773871ab118e2f8b3fcb2209b
timothymullen <timothy.liam.mullen@gmail.com>
2018-11-15 02:45:29 -0500

    more updates

--- game.js
+++ game.js
@@ -12,42 +12,51 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   road = new Path();
   context_switcher = new ContextSwitcher();
+  noFill();
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    if (context_switcher.state === 'start') {
+      context_switcher.start_frame = frameCount;
+    }
+    context_switcher.state = 'play';
+  }
+  if (keyCode === RIGHT_ARROW) {
+    context_switcher.state = 'pause';
+  }
+
 }
 
 function draw() {
-  background('#619f3f')
-  noFill();
-  road.draw()
+  if (frameCount === 1) {
+    background('#619f3f')
+    road.draw();
+  }
+
+  if (context_switcher.state === 'play') {
+    background('#619f3f')
+    road.draw()
+  }
 }
 
 class ContextSwitcher {
   constructor() {
     this.state = 'start';
+    this.start_frame = 0;
   }
 }
 
 class PathSegment {
-  constructor(min, max, unit) {
-    /*
-    Pretty much just a wrapper for a Bezier curve.
-    */
-    const start_coord = min * unit;
-    const end_coord   = max * unit;
-    const controls = [start_coord + Math.random() * unit,
-      start_coord + Math.random() * unit,
-      start_coord + Math.random() * unit,
-      start_coord + Math.random() * unit
-    ]
-
-    //const bez = new Bezier(start_coord, start_coord, controls[0], controls[1], controls[2], controls[3], end_coord, end_coord);
-    this.x1 = start_coord;
-    this.y1 = start_coord;
-    this.x2 = controls[0];
-    this.y2 = controls[1];
-    this.x3 = controls[2];
-    this.y3 = controls[3];
-    this.x4 = end_coord;
-    this.y4 = end_coord;
+  constructor(x1, y1, x2, y2, x3, y3, x4, y4) {
+    this.x1 = x1
+    this.y1 = y1
+    this.x2 = x2
+    this.y2 = y2
+    this.x3 = x3
+    this.y3 = y3
+    this.x4 = x4
+    this.y4 = y4
   }
 
   draw() {
@@ -63,6 +72,21 @@ class PathSegment {
     stroke(255, 102, 0);
     cowboy.blit(x, y);
   }
+}
+
+
+function tile(i, unit) {
+    /*
+    Creates an i * unit by i * unit downwardly descending tile
+    */
+    const start_coord = i * unit;
+    const end_coord   = (i + 1) * unit;
+    const controls = [start_coord + Math.random() * unit,
+      start_coord + Math.random() * unit,
+      start_coord + Math.random() * unit,
+      start_coord + Math.random() * unit
+    ]
+    return new PathSegment(start_coord, start_coord, controls[0], controls[1], controls[2], controls[3], end_coord, end_coord);
 }
 
 class Map {
@@ -83,8 +107,7 @@ class Path {
     this.curves = [];
 
     for (let i=0; i<this.num_curves; i++) {
-      const new_curve = new PathSegment(i, i+1, this.unit);
-      //const bez = new Bez((i+1) * unit, (i + 2) * unit)
+      const new_curve = tile(i, this.unit);
       this.curves.push(new_curve);
     }
   }
@@ -92,35 +115,19 @@ class Path {
   draw() {
     for (let a_curve of this.curves) {
       a_curve.draw();
-      /*
-      stroke('#000');
-      ellipse(a_curve.x1, a_curve.y1, 5, 5);
-      ellipse(a_curve.x4, a_curve.y4, 5, 5);
-      stroke('#fff');
-      ellipse(a_curve.x2, a_curve.y2, 5, 5);
-      ellipse(a_curve.x3, a_curve.y3, 5, 5);
-      */
     }
 
 
     /* now blit the cowboy */
     const divisor        = this.num_curves * this.num_segments;
 
-    const frame_counter  = Math.floor(frameCount / 1.6)
+    const frame_counter  = Math.floor((frameCount - context_switcher.start_frame) / 1.6)
     const position       = frame_counter % divisor;
     const position_index = position % this.num_segments;
     const curve_index    = Math.floor(position / this.num_segments);
 
-    //console.table(divisor, position, curve_index, position_index);
+    //console.table(divisor, frame_counter, position, curve_index, position_index);
     this.curves[curve_index].follow(position_index / this.num_segments);
-
-    /*
-    for (a_curve of curves) {
-    for (let i = 0; i < 30; i++) {
-    curve.follow(i/30);
-    }
-    }
-    */
   }
 }
 
@@ -137,8 +144,39 @@ class Cowboy {
     image(this.image, x - (this.image.width / 2), y - (this.image.height / 2))
   }
 }
-function drawPath(i) {
-  const W = 90;
+
+function new_tile(i, unit, orientation) {
+    const top_left = i * unit;
+    const bottom_right   = (i + 1) * unit;
+    const controls = [
+      top_left + Math.random() * unit,
+      top_left + Math.random() * unit,
+      top_left + Math.random() * unit,
+      top_left + Math.random() * unit
+    ]
+    const start_x, start_y, end_x, end_y;
+    switch(orientation) {
+      case 'se':
+        start_x = top_left;
+        start_y = top_left;
+        end_x   = bottom_right;
+        end_y   = bottom_right;
+        break;
+      case 's':
+        start_x = top_left;
+        start_y = top_left;
+        end_x   = top_left;
+        end_y   = bottom_right;
+        break;
+      case 'sw':
+        start_x = bottom_right;
+        start_y = top_left;
+        end_x   = top_left;
+        end_y   = bottom_right;
+        break;
+    }
+
+    return new PathSegment(start_x, start_y, controls[0], controls[1], controls[2], controls[3], end_x, end_y);
 }
 
 /*
